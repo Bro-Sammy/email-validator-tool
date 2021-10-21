ae13db9c001cbe0b0868a154a556c189f4dc302e
Samuel <skwaku6@gmail.com>
2021-10-21 14:06:52 +0000

    Fix function response error code for .edu.ng livingseed

--- pages/api/validator.js
+++ pages/api/validator.js
@@ -25,7 +25,7 @@ export default async function Validate(req, res) {
                 } else {
                     return res.json({ ErrorMsg: "Invalid Email 🚫", result: [validMailbox, result] })
                 }
-            } else if(result===false && validMailbox===true){
+            } else if(result===false && validMailbox===true || null){
                 // console.log(result, validMailbox)
                 return res.json({  SuccessMsg: "Valid Email 📬", statusCode: res.statusCode, result: validMailbox  })
             }else {
